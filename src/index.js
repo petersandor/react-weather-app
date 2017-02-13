e8e4d6181c5261e54f031647b937d43d3030c459
Peter Sandor <peter@petersandor.name>
2017-02-13 20:08:02 +0100

    Use hashHistory in router when served on GH pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { browserHistory } from 'react-router';
+import { hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
@@ -10,7 +10,7 @@ import configureStore from './store/configureStore';
 import './index.css';
 
 const store = configureStore();
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(hashHistory, store);
 
 
 const rootElement = document.getElementById('root');
